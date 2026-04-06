bf60330c0cb432d00f4733ef75a1d02b082b6b3c
DarkFox Studios <105975824+DarkFoxYT@users.noreply.github.com>
2026-04-06 14:49:33 +0200

    Fix 1.21.2 loader compatibility and dev runtime

--- common/src/main/java/com/flubburr/aioa/spawn/ApocalypseSpawnManager.java
+++ common/src/main/java/com/flubburr/aioa/spawn/ApocalypseSpawnManager.java
@@ -13,6 +13,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
@@ -211,7 +212,7 @@ public final class ApocalypseSpawnManager {
             return false;
         }
 
-        Entity entity = entityType.create(level);
+        Entity entity = entityType.create(level, EntitySpawnReason.EVENT);
         if (!(entity instanceof Mob mob)) {
             return false;
         }
@@ -256,7 +257,7 @@ public final class ApocalypseSpawnManager {
         }
 
         ResourceLocation biomeId = level.registryAccess()
-                .registryOrThrow(Registries.BIOME)
+                .lookupOrThrow(Registries.BIOME)
                 .getKey(level.getBiome(pos).value());
 
         if (biomeId == null) {
