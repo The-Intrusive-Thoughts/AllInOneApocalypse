8307a51655f608a886eecce542789d79cf7a9127
DarkFox <darkfoxonyt@gmail.com>
2026-08-02 22:30:37 +0200

    Release 0.2.0 mob studio and polish

--- fabric/src/main/java/com/flubburr/aioa/fabric/AioaFabric.java
+++ fabric/src/main/java/com/flubburr/aioa/fabric/AioaFabric.java
@@ -3,6 +3,12 @@ package com.flubburr.aioa.fabric;
 import com.flubburr.aioa.AioaCommon;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+
+import com.flubburr.aioa.AioaConstants;
+import com.flubburr.aioa.network.AioaSpawnRequest;
+import com.flubburr.aioa.spawn.AioaSpawnStudioHandler;
 
 public final class AioaFabric implements ModInitializer {
 
@@ -10,5 +16,9 @@ public final class AioaFabric implements ModInitializer {
     public void onInitialize() {
         AioaCommon.init();
         ServerTickEvents.END_WORLD_TICK.register(AioaCommon::onServerLevelTick);
+        PayloadTypeRegistry.playC2S().register(AioaFabricSpawnPayload.TYPE, AioaFabricSpawnPayload.CODEC);
+        ServerPlayNetworking.registerGlobalReceiver(AioaFabricSpawnPayload.TYPE, (payload, context) -> {
+            context.server().execute(() -> AioaSpawnStudioHandler.handle(context.player(), payload.request()));
+        });
     }
 }
