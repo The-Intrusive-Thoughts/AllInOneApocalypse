07032be5a98d3d1c4a3083aae9e0ed8e4baf3f91
DarkFox <darkfoxonyt@gmail.com>
2026-08-03 23:38:35 +0200

    Fix Forge sounds, guide persistence, and spawning

--- common/src/main/java/com/flubburr/aioa/behavior/AioaBehaviorRuntime.java
+++ common/src/main/java/com/flubburr/aioa/behavior/AioaBehaviorRuntime.java
@@ -2,6 +2,7 @@ package com.flubburr.aioa.behavior;
 
 import com.flubburr.aioa.AioaConstants;
 import com.flubburr.aioa.compat.AioaEntityHelper;
+import com.flubburr.aioa.compat.AioaRegistryCompat;
 import com.flubburr.aioa.config.AioaConfigManager;
 import com.flubburr.aioa.spawn.AioaZombieBehaviour;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -508,7 +509,7 @@ public final class AioaBehaviorRuntime {
 
     private static void playSound(AioaBehaviorGraph.Node node, Mob mob) {
         Optional<ResourceLocation> id = Optional.ofNullable(AioaEntityHelper.parseResourceLocation(node.parameters.getOrDefault("sound", "minecraft:entity.zombie.ambient")));
-        SoundEvent sound = id.map(BuiltInRegistries.SOUND_EVENT::getValue).orElse(null);
+        SoundEvent sound = id.map(AioaRegistryCompat::getSoundEvent).orElse(null);
         if (sound != null) {
             mob.level().playSound(null, mob.blockPosition(), sound, SoundSource.HOSTILE,
                     (float) number(node, "volume", 1, 0, 4), (float) number(node, "pitch", 1, 0.25, 2));
