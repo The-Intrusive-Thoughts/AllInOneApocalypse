c5fe3377c9247a4d922bb6b2ec244c3c4ed7e199
DarkFox <darkfoxonyt@gmail.com>
2026-08-08 23:37:12 +0200

    Adapt visual picker across Minecraft UI APIs

--- common/src/main/java/com/flubburr/aioa/client/config/AioaItemPickerScreen.java
+++ common/src/main/java/com/flubburr/aioa/client/config/AioaItemPickerScreen.java
@@ -4,6 +4,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
@@ -81,7 +82,10 @@ final class AioaItemPickerScreen extends AioaAnimatedScreen {
     }
 
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+    public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+        double mouseX = event.x();
+        double mouseY = event.y();
+        int button = event.button();
         if (button == 0 && mouseX >= this.gridLeft && mouseX < this.gridLeft + COLUMNS * CELL
                 && mouseY >= this.gridTop && mouseY < this.gridTop + ROWS * CELL) {
             int column = ((int) mouseX - this.gridLeft) / CELL;
@@ -98,7 +102,7 @@ final class AioaItemPickerScreen extends AioaAnimatedScreen {
                 return true;
             }
         }
-        return super.mouseClicked(mouseX, mouseY, button);
+        return super.mouseClicked(event, doubleClick);
     }
 
     private void confirm() {
@@ -136,14 +140,22 @@ final class AioaItemPickerScreen extends AioaAnimatedScreen {
         super.render(graphics, mouseX, mouseY, partialTick);
         if (hoveredIndex >= 0 && hoveredIndex < this.filteredItems.size()) {
             Identifier id = this.filteredItems.get(hoveredIndex);
-            graphics.renderTooltip(this.font, Component.literal(itemName(id) + "\n" + id), mouseX, mouseY);
+            String tooltipName = itemName(id);
+            String registryName = id.toString();
+            int tooltipWidth = Math.max(this.font.width(tooltipName), this.font.width(registryName)) + 12;
+            int tooltipX = Math.max(4, Math.min(this.width - tooltipWidth - 4, mouseX + 10));
+            int tooltipY = Math.max(4, Math.min(this.height - 34, mouseY + 10));
+            graphics.fill(tooltipX, tooltipY, tooltipX + tooltipWidth, tooltipY + 31, 0xF5111814);
+            graphics.fill(tooltipX, tooltipY, tooltipX + tooltipWidth, tooltipY + 1, 0xFF6EFFBA);
+            graphics.drawString(this.font, tooltipName, tooltipX + 6, tooltipY + 5, 0xFFE2F8E9);
+            graphics.drawString(this.font, registryName, tooltipX + 6, tooltipY + 17, 0xFF8CB79B);
         }
         this.finishUiRender(graphics);
     }
 
     private static String itemName(Identifier id) {
         Item item = BuiltInRegistries.ITEM.getValue(id);
-        return item == null ? id.getPath() : item.getDescription().getString();
+        return item == null ? id.getPath() : new ItemStack(item).getHoverName().getString();
     }
 
     @Override
